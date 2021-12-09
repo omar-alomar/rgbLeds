aacee1a6afa7b278a093d2326b130b9815829d9a
Omar Alomar <omaro2000@gmail.com>
2021-12-08 19:59:47 -0500

    rainbow loop

--- rgbLeds.ino
+++ rgbLeds.ino
@@ -1,9 +1,26 @@
 #include <FastLED.h>
 #define LED_PIN 2
-#define NUM_LEDS 12
+#define NUM_LEDS 124
 
 CRGB leds[NUM_LEDS];
 
-viod setup() {
-    FastLED.addLeds<WS2812, LED_PIN, RGB
+uint8_t hue = 0;
+
+void setup() {
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+    
+    FastLED.setBrightness(250);
+}
+
+void loop() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    //leds[i] = CHSV(hue, 255, 255);
+    leds[i] = CHSV(hue + (i*5), 255, 255);
+  }
+
+  EVERY_N_MILLISECONDS(8){
+    hue++;
+  }
+
+  FastLED.show();
 }
